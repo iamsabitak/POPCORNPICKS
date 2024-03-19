46c30d0b391ef8edea4a4aef57174b29175f79a0
sabita <sabitakhadka091@gmail.com>
2024-03-19 13:25:18 +0545

    Configured routes for Home, Movie, and Header components using the react-router-dom

--- src/AppContent.tsx
+++ src/AppContent.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Movie from "./components/Movie";
+import Header from "./components/Header";
 
 const AppContent: React.FC = () => {
   return (
     <>
       <h1>POPCORNPICKS - AppContent</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/movie" element={<Movie />} />
+          <Route path="/header" element={<Header />} />
+        </Routes>
+      </Router>
     </>
   );
 };
