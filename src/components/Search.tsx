5debf242b256d71e48fee3c88ee8136e5d6eb219
sabita <sabitakhadka091@gmail.com>
2024-03-19 17:30:37 +0545

    Added handleSubmit function to handle form submission and prevent default behavior.

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,32 +1,37 @@
-import { TextInput } from "@mantine/core";
-
-import React from "react";
+import React, { useState } from "react";
 
 const Search: React.FC = () => {
- 
-  
+  const [query, setQuery] = useState<string>("pokemon");
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+  };
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setQuery(query);
+  };
   return (
-    <>
-      <TextInput
-        placeholder="Search your favourite movie....."
-        styles={{
-          input: {
-            border: "1px solid grey",
-            width: "40rem",
-            height: "2.5rem",
-            fontSize: "1rem",
-            borderRadius: "5px",
-            marginTop: "15px",
-            marginBottom: "15px",
-            textAlign: "center",
-            backgroundColor: "#1c1c1c",
-            color: "#fff",
-            WebkitTextFillColor: "#fff",
-            fontFamily: "Courier, monospace",
-          },
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="Search your favorite movie..."
+        onChange={handleChange}
+        value={query}
+        style={{
+          border: "1px solid grey",
+          width: "48rem",
+          height: "2.5rem",
+          fontSize: "1rem",
+          borderRadius: "5px",
+          marginTop: "15px",
+          marginBottom: "15px",
+          textAlign: "center",
+          backgroundColor: "#1c1c1c",
+          color: "#fff",
+          WebkitTextFillColor: "#fff",
+          fontFamily: "Courier, monospace",
+          marginRight: "6rem",
         }}
       />
-    </>
+    </form>
   );
 };
 
