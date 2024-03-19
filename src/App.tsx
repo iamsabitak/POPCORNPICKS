ebb1b55c2e1c383a6b5db12cdfeb57c63e3cae86
sabita <sabitakhadka091@gmail.com>
2024-03-19 12:58:24 +0545

    implemented the appcontent component in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import AppContent from "./AppContent";
 
 const App: React.FC = () => {
   return (
     <>
       <h1>MOVIE SEARCHING APP</h1>
+      <AppContent />
     </>
   );
 };
