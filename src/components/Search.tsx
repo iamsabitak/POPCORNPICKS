1dc35d6350704e6b74a69b5e378a28f6edad1300
sabita <sabitakhadka091@gmail.com>
2024-03-19 17:33:59 +0545

    implement textinput from mantine

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { TextInput } from "@mantine/core";
 
 const Search: React.FC = () => {
   const [query, setQuery] = useState<string>("pokemon");
@@ -11,24 +12,26 @@ const Search: React.FC = () => {
   };
   return (
     <form onSubmit={handleSubmit}>
-      <input
+      <TextInput
         placeholder="Search your favorite movie..."
         onChange={handleChange}
         value={query}
-        style={{
-          border: "1px solid grey",
-          width: "48rem",
-          height: "2.5rem",
-          fontSize: "1rem",
-          borderRadius: "5px",
-          marginTop: "15px",
-          marginBottom: "15px",
-          textAlign: "center",
-          backgroundColor: "#1c1c1c",
-          color: "#fff",
-          WebkitTextFillColor: "#fff",
-          fontFamily: "Courier, monospace",
-          marginRight: "6rem",
+        styles={{
+          input: {
+            border: "1px solid grey",
+            width: "48rem",
+            height: "2.5rem",
+            fontSize: "1rem",
+            borderRadius: "5px",
+            marginTop: "15px",
+            marginBottom: "15px",
+            textAlign: "center",
+            backgroundColor: "#1c1c1c",
+            color: "#fff",
+            WebkitTextFillColor: "#fff",
+            fontFamily: "Courier, monospace",
+            marginRight: "6rem",
+          },
         }}
       />
     </form>
