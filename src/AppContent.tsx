9ed39f6a4dc869e731b41efa3d507a29281d4679
sabita <sabitakhadka091@gmail.com>
2024-03-19 13:28:19 +0545

    import the home component

--- src/AppContent.tsx
+++ src/AppContent.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Movie from "./components/Movie";
 import Header from "./components/Header";
+import Home from "./components/Home";
 
 const AppContent: React.FC = () => {
   return (
