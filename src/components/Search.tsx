87d115235e408c107c42d1a1b86805c2fa431057
sabita <sabitakhadka091@gmail.com>
2024-03-19 16:01:06 +0545

    style the textinput component

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -3,9 +3,29 @@ import { TextInput } from "@mantine/core";
 import React from "react";
 
 const Search: React.FC = () => {
+ 
+  
   return (
     <>
-      <TextInput placeholder="Search your fav movie....." />
+      <TextInput
+        placeholder="Search your favourite movie....."
+        styles={{
+          input: {
+            border: "1px solid grey",
+            width: "40rem",
+            height: "2.5rem",
+            fontSize: "1rem",
+            borderRadius: "5px",
+            marginTop: "15px",
+            marginBottom: "15px",
+            textAlign: "center",
+            backgroundColor: "#1c1c1c",
+            color: "#fff",
+            WebkitTextFillColor: "#fff",
+            fontFamily: "Courier, monospace",
+          },
+        }}
+      />
     </>
   );
 };
