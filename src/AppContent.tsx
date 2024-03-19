38d6477a137bf58eaeedd0c8bab1e7c1b4101499
sabita <sabitakhadka091@gmail.com>
2024-03-19 22:14:07 +0545

    Configured routes for displaying movie details

--- src/AppContent.tsx
+++ src/AppContent.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Movie from "./components/Movie";
 
 import Navbar from "./components/Navbar";
+import MovieDetails from "./components/MovieDetails";
 
 const AppContent: React.FC = () => {
   return (
@@ -12,6 +13,7 @@ const AppContent: React.FC = () => {
         <Navbar />
         <Routes>
           <Route path="/" element={<Movie query="pokemon" />} />
+          <Route path="/movies/:imdbID" element={<MovieDetails />} />
         </Routes>
       </Router>
     </>
