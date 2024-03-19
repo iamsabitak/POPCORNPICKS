092ec6c99d7cf1dd397e9fb869381df8e451e898
sabita <sabitakhadka091@gmail.com>
2024-03-19 15:44:38 +0545

    added the textinput from mantine

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,11 +1,11 @@
-
+import { TextInput } from "@mantine/core";
 
 import React from "react";
 
 const Search: React.FC = () => {
   return (
     <>
-     
+      <TextInput placeholder="Search your fav movie....." />
     </>
   );
 };
