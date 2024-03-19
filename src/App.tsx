90b80e8b866b5e1d3f50bf2cc8edeeadce26bf4a
sabita <sabitakhadka091@gmail.com>
2024-03-19 13:16:57 +0545

    removed the browserrouter from the app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import AppContent from "./AppContent";
 import { MantineProvider } from "@mantine/core";
-import { BrowserRouter } from "react-router-dom";
 
 const App: React.FC = () => {
   return (
     <>
-      <BrowserRouter>
-        <MantineProvider>
-          <AppContent />
-        </MantineProvider>
-      </BrowserRouter>
+      <MantineProvider>
+        <AppContent />
+      </MantineProvider>
     </>
   );
 };
