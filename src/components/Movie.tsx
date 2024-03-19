929520a0bb62b18b135611b63af9dcb897a6965e
sabita <sabitakhadka091@gmail.com>
2024-03-19 15:01:13 +0545

    Implement fetching movie data from OMDB API

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Movie: React.FC = () => {
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "http://www.omdbapi.com/?apikey=cb2dbd2d&s=doraemon"
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        console.log(data.Search);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <>
       <h1>POPCORNPICKS - MOVIE</h1>
