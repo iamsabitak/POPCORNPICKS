d3bc8f22fc18021a6c69f886c8db3f44d3221f08
sabita <sabitakhadka091@gmail.com>
2024-03-19 13:42:38 +0545

    added some inline css

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,67 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { NavLink } from "@mantine/core";
 
 const Navbar: React.FC = () => {
   return (
     <>
-      <h1>POPCORNPICKS - NAVBAR</h1>
+      <nav
+        style={{
+          backgroundColor: "#151313",
+          padding: "0px 20px",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          height: "4rem",
+        }}
+      >
+        <NavLink
+          label="POPCORNPICKS 🍿"
+          to="/"
+          component={Link}
+          style={{
+            textDecoration: "none",
+            color: "#ffa500",
+            fontSize: "18px",
+            fontWeight: "bold",
+          }}
+        />
+        <ul
+          style={{
+            listStyle: "none",
+            margin: "0",
+            padding: "0",
+            display: "flex",
+          }}
+        >
+          <li style={{ marginLeft: "15px" }}>
+            <NavLink
+              label="Home"
+              to="/"
+              component={Link}
+              style={{
+                textDecoration: "none",
+                color: "#fff",
+                fontSize: "17px",
+                transition: "color 0.3s ease",
+              }}
+            />
+          </li>
+          <li style={{ marginLeft: "15px" }}>
+            <NavLink
+              label="Movie"
+              to="/movie"
+              component={Link}
+              style={{
+                textDecoration: "none",
+                color: "#fff",
+                fontSize: "17px",
+                transition: "color 0.3s ease",
+              }}
+            />
+          </li>
+        </ul>
+      </nav>
     </>
   );
 };
