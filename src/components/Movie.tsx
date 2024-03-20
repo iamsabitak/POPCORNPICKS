c8bacfe6e01849d018f3d62b6484638e6c839ec6
sabita <sabitakhadka091@gmail.com>
2024-03-20 10:31:45 +0545

    removed the unnecessary code

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -58,68 +58,69 @@ const Movie: React.FC<{ query: string }> = ({ query }) => {
           Error: {error}
         </div>
       )}
-      {movies.length > 0 && (
-        <>
-          <Title style={{ textAlign: "center", color: "#08e408" }}>
-            POPCORNPICKS - MOVIES
-          </Title>
-          <div
-            style={{
-              display: "flex",
-              flexWrap: "wrap",
-              gap: "20px",
-              justifyContent: "center",
-            }}
-          >
-            {movies.map((movie) => {
-              const movieTitle = movie.Title.substring(0, 16);
-              return (
-                <div key={movie.imdbID}>
-                  <Card
+      <>
+        <Title style={{ textAlign: "center", color: "#08e408" }}>
+          POPCORNPICKS - MOVIES
+        </Title>
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            gap: "20px",
+            justifyContent: "center",
+          }}
+        >
+          {movies.map((movie) => {
+            const movieTitle = movie.Title.substring(0, 16);
+            return (
+              <div key={movie.imdbID}>
+                <Card
+                  style={{
+                    width: "250px",
+                    textAlign: "center",
+                    margin: "10px",
+                    backgroundColor: "#151313",
+                    borderRadius: "10px",
+                  }}
+                >
+                  <Image
+                    src={movie.Poster}
+                    alt={movieTitle}
                     style={{
+                      marginBottom: "10px",
                       width: "250px",
-                      textAlign: "center",
-                      margin: "10px",
-                      backgroundColor: "#151313",
-                      borderRadius: "10px",
+                      height: "300px",
+                      objectFit: "cover",
+                      borderTopRightRadius: "10px",
+                      borderTopLeftRadius: "10px",
                     }}
-                  >
-                    <Image
-                      src={movie.Poster}
-                      alt={movieTitle}
+                  />
+                  <h3>
+                    {" "}
+                    {movieTitle.length > 15 ? `${movieTitle}...` : movieTitle}
+                  </h3>
+                  <Link key={movie.imdbID} to={`/movies/${movie.imdbID}`}>
+                    <Button
                       style={{
-                        marginBottom: "10px",
-                        width: "250px",
-                        height: "300px",
-                        objectFit: "cover",
-                        borderTopRightRadius: "10px",
-                        borderTopLeftRadius: "10px",
+                        color: "#fff",
+                        cursor: "pointer",
+                        border: "1px solid #08e408",
+                        backgroundColor: "#08e408",
+                        borderRadius: "4px",
+                        height: "2rem",
+                        width: "5rem",
+                        marginBottom: "1rem",
                       }}
-                    />
-                    <h3> {movieTitle.length > 15 ? `${movieTitle}...` : movieTitle}</h3>
-                    <Link key={movie.imdbID} to={`/movies/${movie.imdbID}`}>
-                      <Button
-                        style={{
-                          color: "#fff",
-                          cursor: "pointer",
-                          border: "1px solid #08e408",
-                          backgroundColor: "#08e408",
-                          borderRadius: "4px",
-                          height: "2rem",
-                          width: "5rem",
-                          marginBottom: "1rem",
-                        }}
-                      >
-                        Details
-                      </Button>
-                    </Link>
-                  </Card>
-                </div>
-              );
-            })}
-          </div>
-        </>
-      )}
+                    >
+                      Details
+                    </Button>
+                  </Link>
+                </Card>
+              </div>
+            );
+          })}
+        </div>
+      </>
     </>
   );
 };
