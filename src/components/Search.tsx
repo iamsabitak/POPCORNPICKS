102192092ae404573d1533307ce8ec1a46e5457d
sabita <sabitakhadka091@gmail.com>
2024-03-20 15:46:56 +0545

    removed the unnecessary handlesubmit

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -9,37 +9,32 @@ const Search: React.FC = () => {
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(e.target.value);
   };
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setQuery(query);
-  };
+
   return (
-    <form onSubmit={handleSubmit}>
-      <TextInput
-        ref={inputEl}
-        type="text"
-        placeholder="Search your favorite movie..."
-        onChange={handleChange}
-        value={query}
-        styles={{
-          input: {
-            border: "1px solid grey",
-            width: "48rem",
-            height: "2.5rem",
-            fontSize: "1rem",
-            borderRadius: "5px",
-            marginTop: "15px",
-            marginBottom: "15px",
-            textAlign: "center",
-            backgroundColor: "#1c1c1c",
-            color: "#fff",
-            WebkitTextFillColor: "#fff",
-            fontFamily: "Courier, monospace",
-            marginRight: "6rem",
-          },
-        }}
-      />
-    </form>
+    <TextInput
+      ref={inputEl}
+      type="text"
+      placeholder="Search your favorite movie..."
+      onChange={handleChange}
+      value={query}
+      styles={{
+        input: {
+          border: "1px solid grey",
+          width: "48rem",
+          height: "2.5rem",
+          fontSize: "1rem",
+          borderRadius: "5px",
+          marginTop: "15px",
+          marginBottom: "15px",
+          textAlign: "center",
+          backgroundColor: "#1c1c1c",
+          color: "#fff",
+          WebkitTextFillColor: "#fff",
+          fontFamily: "Courier, monospace",
+          marginRight: "6rem",
+        },
+      }}
+    />
   );
 };
 
