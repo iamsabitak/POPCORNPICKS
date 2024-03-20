1a1e9c3a2b44476c38e31354c5e79f8cfd5bd598
sabita <sabitakhadka091@gmail.com>
2024-03-20 11:16:04 +0545

    wrapped the appcontent from movieprovider to provide the value

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import AppContent from "./components/AppContent";
 import { MantineProvider } from "@mantine/core";
+import { MovieProvider } from "./usecontex/MovieContext";
 
 const App: React.FC = () => {
   return (
     <>
+    <MovieProvider>
       <MantineProvider>
         <AppContent />
       </MantineProvider>
+      </MovieProvider>
     </>
   );
 };
