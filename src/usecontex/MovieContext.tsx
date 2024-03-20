21e00b83ca0ca5ccc79003eafca918e533c533db
sabita <sabitakhadka091@gmail.com>
2024-03-20 11:18:36 +0545

    Implement TypeScript support for MovieContext and useMovieContext

---
+++ src/usecontex/MovieContext.tsx
@@ -0,0 +1,26 @@
+import React, { createContext, ReactNode, useState } from "react";
+interface MovieContextType {
+  query: string;
+  setQuery: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const MovieContext = createContext<MovieContextType | undefined>(undefined);
+
+export const MovieProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [query, setQuery] = useState<string>("pokemon");
+
+  const contextValue: MovieContextType = {
+    query,
+    setQuery,
+  };
+
+  return (
+    <MovieContext.Provider value={contextValue}>
+      {children}
+    </MovieContext.Provider>
+  );
+};
+
+export { MovieContext };
