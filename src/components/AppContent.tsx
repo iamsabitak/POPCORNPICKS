2d0edcbd56baffa52cc39e729dc7675fc1552105
sabita <sabitakhadka091@gmail.com>
2024-03-20 11:14:10 +0545

    import the query from usemoviecontex

--- src/components/AppContent.tsx
+++ src/components/AppContent.tsx
@@ -5,14 +5,16 @@ import Movie from "./Movie";
 
 import Navbar from "./Navbar";
 import MovieDetails from "./MovieDetails";
+import { useMovieContext } from "../usecontex/useMovieContext";
 
 const AppContent: React.FC = () => {
+  const { query } = useMovieContext();
   return (
     <>
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Movie query="pokemon" />} />
+          <Route path="/" element={<Movie query={query} />} />
           <Route path="/movies/:imdbID" element={<MovieDetails />} />
         </Routes>
       </Router>
