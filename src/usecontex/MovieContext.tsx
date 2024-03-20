0623c79764ff4f4a90983a7545cbd618cbca653c
sabita <sabitakhadka091@gmail.com>
2024-03-20 12:52:01 +0545

    Add state for managing movies and its details

--- src/usecontex/MovieContext.tsx
+++ src/usecontex/MovieContext.tsx
@@ -3,7 +3,27 @@ import React, { createContext, ReactNode, useState } from "react";
 export interface MovieContextType {
   query: string;
   setQuery: React.Dispatch<React.SetStateAction<string>>;
+  movies: Movie[];
+  setMovies: React.Dispatch<React.SetStateAction<Movie[]>>;
+  loading: boolean;
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
+  error: string | null;
+  setError: React.Dispatch<React.SetStateAction<string | null>>;
+  moviesDetails: Movie | null;
+  setMoviesDetails: React.Dispatch<React.SetStateAction<Movie | null>>;
 }
+
+export interface Movie {
+  Title: string;
+  Year: string;
+  Poster: string;
+  Type?: string;
+  imdbID: string;
+}
+
+const initialMovieState: Movie[] = [];
+const initialLoadingState: boolean = false;
+const initialErrorState: string | null = null;
 
 const MovieContext = createContext<MovieContextType | undefined>(undefined);
 
@@ -11,10 +31,22 @@ export const MovieProvider: React.FC<{ children: ReactNode }> = ({
   children,
 }) => {
   const [query, setQuery] = useState<string>("pokemon");
+  const [movies, setMovies] = useState<Movie[]>(initialMovieState);
+  const [loading, setLoading] = useState<boolean>(initialLoadingState);
+  const [error, setError] = useState<string | null>(initialErrorState);
+  const [moviesDetails, setMoviesDetails] = useState<Movie | null>(null);
 
   const contextValue: MovieContextType = {
     query,
     setQuery,
+    movies,
+    setMovies,
+    loading,
+    setLoading,
+    error,
+    setError,
+    moviesDetails,
+    setMoviesDetails,
   };
 
   return (
